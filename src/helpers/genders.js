dd7a5c07dbc64729f151a36de9097a8d256956d1
KevinMendieta <kevin@wonderschool.com>
2022-05-08 11:24:10 -0500

    add more options to gender field

--- src/helpers/genders.js
+++ src/helpers/genders.js
@@ -1,6 +1,8 @@
 // genders list
 export const GENDERS = [
-  { id: "g121", value: "M", label: "Male" },
-  { id: "g122", value: "F", label: "Female" },
-  { id: "g123", value: "O", label: "Other" },
+  { id: "g121", value: undefined, label: "Select one" },
+  { id: "g122", value: "M", label: "Male" },
+  { id: "g123", value: "F", label: "Female" },
+  { id: "g124", value: "NB", label: "Non binary" },
+  { id: "g125", value: "O", label: "Other" },
 ];
